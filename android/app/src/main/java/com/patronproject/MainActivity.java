aa9e0ccab6905a9b79d19fb466271a42eecfa576
Akunambi <aburnz65@gmail.com>
2023-05-13 14:35:23 +0300

    aab

--- android/app/src/main/java/com/patronproject/MainActivity.java
+++ android/app/src/main/java/com/patronproject/MainActivity.java
@@ -1,4 +1,4 @@
-package com.patrondev.patronlasers;
+package com.patronproject;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/patronproject/MainApplication.java
+++ android/app/src/main/java/com/patronproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.patrondev.patronlasers;
+package com.patronproject;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
