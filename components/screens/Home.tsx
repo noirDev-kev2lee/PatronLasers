57fc270e59920900ee9cd99e7d507d657018d830
Hatibu <juliushatibu@gmail.com>
2023-06-01 17:42:11 +0300

    minor changes

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   pressTxt: {
-    fontFamily: 'Inter',
+    fontFamily: 'Roboto',
     textTransform: 'uppercase',
     letterSpacing: 2,
     fontSize: 17,
